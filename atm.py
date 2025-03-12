b619bbc393d99e29502bb7a81a37e3084ad0db05
yangjiun00 <jiunine4@gmail.com>
2025-03-12 18:05:50 +0900

    ✨ 입금액 추가
    사용자 입금액을 받고 원금에 추가하는 기능, 입금액 잔액 출력 기능 추가

--- atm.py
+++ atm.py
@@ -1,6 +1,26 @@
 #balance : 통장 처음 만들 때 들어있는 기본금액
 #1. 입금 / 2. 출금 / 3. 영수증 보기 / 4. 종료
+#deposit_amount : 
 
 balance = 3000
 
-num = input("사용할 서비스를 선택해주세요 (입금, 출금, 영수증 보기, 종료) :")
+while True:
+    num = input("사용할 서비스를 선택해주세요 (입금, 출금, 영수증 보기, 종료) :")
+
+    if num == "입금" :
+       deposit_amount = input("입금할 금액을 입력해주세요 :")
+       if deposit_amount.isdigit() and int(deposit_amount) > 0:
+           balance += int(deposit_amount)
+           print(f'입금하신 금액은{deposit_amount}원이고, 입금 후 잔액은{balance}원 입니다.')
+
+       else:
+           print("제대로 된 금액을 입력해주세요.")
+
+       
+    if num == "출금" :
+        pass 
+    if num == "영수증 보기" :
+        pass
+    if num == "종료" :
+        print("서비스를 종료합니다.")
+        break
