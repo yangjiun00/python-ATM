2ad1a74e5bdce03634402446523d1399833ee0ac
yangjiun00 <jiunine4@gmail.com>
2025-03-13 18:19:41 +0900

    ✨ 영수증 내역 추가
    입금 출금 내역을 담는 리스트 추가, 영수증 출력 문구 추

--- atm.py
+++ atm.py
@@ -1,8 +1,14 @@
 #balance : 통장 처음 만들 때 들어있는 기본금액
 #1. 입금 / 2. 출금 / 3. 영수증 보기 / 4. 종료
 #deposit_amount : 
+#영수증 타입은 list
+
+#영수증을 위한 리스트
+check = []
 
 balance = 3000
+
+
 
 while True:
     num = input("사용할 서비스를 선택해주세요 (입금, 출금, 영수증 보기, 종료) :")
@@ -12,6 +18,8 @@ while True:
        if deposit_amount.isdigit() and int(deposit_amount) > 0:
            balance += int(deposit_amount)
            print(f'입금하신 금액은{deposit_amount}원이고, 입금 후 잔액은{balance}원 입니다.')
+
+           check.append(("입금액", deposit_amount, balance)) 
 
        else:
            print("제대로 된 금액을 입력해주세요.")
@@ -26,13 +34,19 @@ while True:
            balance -= int(withdraw_amount)
            print(f'출금하신 금액은{withdraw_amount}원이고, 출금 후 잔액은{balance}원 입니다.')
 
+           check.append(("출금액", withdraw_amount, balance)) 
+
         else:
             print("제대로 된 금액을 입력해주세요.")
         
     if num == "영수증 보기" :
-
-        pass
+        if check:
+            print("===영수증 내역===")
+            for i in check:
+                print(f'{i[0]} : {i[1]}원 | 잔액 : {i[2]}원')
         
+        else:
+            print("내역이 없습니다.")
     if num == "종료" :
         print("서비스를 종료합니다.")
         break
